5c83e75d28b9a7dfbcaf956b4eb9102d368bbcea
Eli66 <immaculartist@gmail.com>
2026-08-19 21:13:01 +0200

    Add adaptation version pins

---
+++ lib/versions.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# Canonical upstream and adaptation pins for Proxmox-LXC-Honcho.
+
+ADAPTATION_VERSION="0.1.0-dev"
+HONCHO_VERSION="3.0.12"
+HONCHO_COMMIT="bd5fd4df62b5002b7aeff6e7f5a5237eb7157260"
+HONCHO_REPOSITORY="https://github.com/plastic-labs/honcho.git"
+UV_VERSION="0.9.24"
+TARGET_DEBIAN_VERSION="13"
+TARGET_ARCH="amd64"
